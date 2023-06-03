6f6f9731127f7ebda19d09de7c85b1f211649640
Akhil Kammila <akhilkammila@gmail.com>
2023-06-03 00:11:02 -0400

    renamed screenshots for ordering

---
+++ premium-screenshotter/rename_screenshots.py
@@ -0,0 +1,16 @@
+"""
+Renames screenshots to be zero-padded by 3 digits
+"""
+
+import os
+
+def rename_screenshots():
+    i = 0
+    for filename in os.listdir('screenshots/1-999'):
+        number = filename.split('. ')[0]
+        title = filename.split('. ')[1]
+        number = number.zfill(3)
+        os.rename('screenshots/1-999/' + filename, 'screenshots/1-999/' + number + '. ' + title)
+
+if __name__ == "__main__":
+    rename_screenshots()
