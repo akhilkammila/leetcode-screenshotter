baf1676208e2cab306b0e2b4bb7cc090cbd87c41
Akhil Kammila <akhilkammila@gmail.com>
2023-06-03 19:38:09 -0400

    moved editorial screenshots into folders

--- premium-screenshotter/rename_screenshots.py
+++ premium-screenshotter/rename_screenshots.py
@@ -16,5 +16,19 @@ def rename_screenshots():
         number = number.zfill(3)
         os.rename('screenshots/1-999/' + filename, 'screenshots/1-999/' + number + '. ' + title)
 
+"""
+Move editorial screenshots into subfolders
+"""
+def move_editorial_screenshots():
+    i = 0
+    for filename in os.listdir('editorial-screenshots'):
+        base = ''
+        if len(filename.split('. ')) < 2: continue
+        number = filename.split('. ')[0]
+        if int(number) < 1000: base = '1-999'
+        elif int(number) < 2000: base = '1000-1999'
+        else: base = '2000-2999'
+        os.rename('editorial-screenshots/' + filename, 'editorial-screenshots/' + base + '/' + filename)
+
 if __name__ == "__main__":
-    rename_screenshots()
+    move_editorial_screenshots()
