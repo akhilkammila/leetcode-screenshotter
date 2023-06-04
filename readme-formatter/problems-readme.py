59a806ca325c5f04ff5549835c9e33e707d8b653
Akhil Kammila <akhilkammila@gmail.com>
2023-06-04 10:35:39 -0400

    added premium probs to readme

---
+++ readme-formatter/problems-readme.py
@@ -0,0 +1,36 @@
+import csv
+
+# Get every row in csv file
+with open('data/problem_data.csv', 'r') as file:
+    csv_reader = csv.reader(file)
+
+    # Convert the CSV reader object to a list of rows
+    rows = list(csv_reader)
+
+# Loop through every row, turn into markdown
+markdownLines = []
+for row in rows[1:]:
+    # Break down row
+    number = row[0]
+    problem_title = row[1]
+    isPremium = row[3]
+    if isPremium != 'TRUE': continue
+
+    # Find subdirectory
+    if int(number) < 1000: subdir = '1-999'
+    elif int(number) < 2000: subdir = '1000-1999'
+    else: subdir = '2000-2999'
+
+    # Find full path
+    base = "https://github.com/akhilkammila/leetcode-screenshotter/tree/main/problem-screenshots/"
+    imagePath = (number.zfill(3) + ". " + problem_title + ".png").replace(" ", "%20")
+    fullPath = base + subdir + "/" + imagePath
+    
+    # Make markdown line
+    markdownLine = "#### [" + number + ". " + problem_title + "](" + fullPath + ")"
+    markdownLines.append(markdownLine)
+
+# Write to readme
+with open('ReadMe.md', 'a') as f:
+    for line in markdownLines:
+        f.write(line + '\n')
